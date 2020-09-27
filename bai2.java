14e67a93cc9dd7a63b5e28aa49c8fcdae53908ea
KhongHeBietQuau <71763190+KhongHeBietQuau@users.noreply.github.com>
2020-09-27 22:47:30 +0700

    Create bai2.java

---
+++ bai2.java
@@ -0,0 +1,38 @@
+
+package bai2;
+
+/**
+ *
+ * @author nguyen manh cuong
+ */
+public class Bai2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("*       *     *     * * * *     * * * *    *     *");
+        System.out.println("|       |    / \\    |       *   |       *   \\   /");
+        System.out.println("* ----- *   *---*   * * * *     * * * *       * ");
+        System.out.println("|       |  /     \\  |           |             |");
+        System.out.println("*       * *       * *           *             *");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("*   *  ****** *       *       *");
+        System.out.println("|\\  |  |       \\     / \\     /");
+        System.out.println("* * *  ******   *   *   *   *");
+        System.out.println("|  \\|  |         \\ /     \\ /");
+        System.out.println("*   *  ******     *       *");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("*     * ******     *     * * * *");
+        System.out.println(" \\   /  |         / \\    |       *");
+        System.out.println("   *    ******   *---*   * * * *");
+        System.out.println("   |    |       /     \\  |    \\ ");
+        System.out.println("   *    ****** *       * *     *");
+    }
+    
+}
