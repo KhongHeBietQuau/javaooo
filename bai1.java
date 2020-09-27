03cc69e69df3334c77482d50b9a1b3ded66fd266
KhongHeBietQuau <71763190+KhongHeBietQuau@users.noreply.github.com>
2020-09-27 22:45:38 +0700

    Create bai1.java

---
+++ bai1.java
@@ -0,0 +1,30 @@
+
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nguyen manh cuong
+ */
+public class runname {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+            int n = input.nextInt();
+            int m = input.nextInt();
+            for (int i = 1; i <= n; i++)
+                if(i==1 || i==n) {
+                    for (int j = 1; j <= m; j++)
+                        System.out.print("* ");
+                    System.out.println();
+                }
+                else {
+                    for(int j=1; j<=m; j++)
+                        if(j==1||j==m)
+                            System.out.print("* ");
+                        else System.out.print("  ");
+                    System.out.println();
+    }
+}
+}
