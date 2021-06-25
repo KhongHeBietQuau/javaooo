a7f35bcd28de3645b7ba7aeb78306e9d37abb47e
KhongHeBietQuau <71763190+KhongHeBietQuau@users.noreply.github.com>
2021-06-26 04:39:07 +0700

    user

--- java/com/hithaui/repositories/BookRepository.java
+++ java/com/hithaui/repositories/BookRepository.java
@@ -3,9 +3,10 @@ package com.hithaui.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.hithaui.dao.BookDAO;
+
+import com.hithaui.dao.BookUpDAO;
 
 @Repository
-public interface BookRepository extends JpaRepository<BookDAO, Integer> {
+public interface BookRepository extends JpaRepository<BookUpDAO, Integer> {
 
 }
